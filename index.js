2784bea51eab69757e27cd6f5ac882c54085e3a4
swedsnayak <swadesh.kumar.nayak@gmail.com>
2019-03-12 10:15:02 +0530

    Optimized Logic

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var readline = require('readline');
 const csv = require('csvtojson')
 
-var csvFilePath = './cities.csv'
 var log = console.log;
+var csvFilePath = './cities.csv'
 
 let citiesList;
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 let data ={};
@@ -19,102 +19,110 @@ data.distributor.subordinates={};
 let child = {};
 child.includes =[];
 child.excludes =[];
+
 let subDistributorCount = 0;
 
 var getIncludedArea = async function () {
 	citiesList = await csv().fromFile(csvFilePath);
 
-  rl.question('Enter Distributor 1 regions to be INCLUDED: ', function (answer) {
-    if (answer == 'next') {
-    	getExcludedArea();
-      //return rl.close();
+ rl.question('\nEnter Distributor-1 regions to be INCLUDED: ', function (answer) {
+  if (answer == 'next') {
+  	getExcludedArea();
+  }
+  else if(answer == 'exit') {
+    log('\nExiting...Thank you!\n');
+    rl.close();
+  }
+  else {
+    log('You have included: ');
+    answer = answer.split('-');
+    var ans = [];
+    if(answer.length == 1) {
+     ans.push("*");
+     ans.push("*");
+     ans.push(answer[0]);
+    }
+    else if(answer.length == 2) {
+     ans.push("*");
+     ans.push(answer[0]);
+     ans.push(answer[1]);
     }
     else {
-	    log('You have included: "', answer, '"');
-	    answer = answer.split('-');
-        var ans = [];
-        if(answer.length == 1) {
-          ans.push("*");
-          ans.push("*");
-          ans.push(answer[0]);
-        }
-        else if(answer.length == 2) {
-          ans.push("*");
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-        }
-        else {
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-          ans.push(answer[2]);
-        }
-        log(ans);
-        let status = false;
-    	for (let i = 0; i < citiesList.length; i++) {
+     ans.push(answer[0]);
+     ans.push(answer[1]);
+     ans.push(answer[2]);
+    }
+    let status = false;
+  	 for (let i = 0; i < citiesList.length; i++) {
 		    if ((citiesList[i]["Country Name"].toUpperCase() == ans[2].toUpperCase() || ans[2]=='*') &&
-		    	(citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
-		    	(citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
-        ){
-		        status = true;
+		  	   (citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
+		  	   (citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
+		  	 ){
+		      status = true;
 		    }
-		}
-		if(status) {
-	  		data.distributor.includes.push(ans);
-		}
-		else {
-			log('Enter valid region!! ');
-		}
-	    getIncludedArea();
-	}
-  });
+		 }
+		 if(status) {
+	 		  data.distributor.includes.push(ans);
+        log(ans);
+		 }
+		 else {
+			   log('Entered region is not valid. Please refer the "cities.csv" for the list of valid regions!');
+		 }
+	  getIncludedArea();
+	  }
+ });
 };
 
 
 var getExcludedArea = function () {
-  rl.question('Enter Distributor 1 regions to be EXCLUDED: ', function (answer) {
-    if (answer == 'next') {
-    	console.log(data);
-    	getResult();
+ rl.question('\nEnter Distributor-1 regions to be EXCLUDED: ', function (answer) {
+  if (answer == 'next') {
+  	getResult();
+  }
+  else if(answer == 'exit') {
+    log('\nExit.\nThank you!\n');
+    rl.close();
+  }
+  else {
+    log('You have excluded: ');
+    answer = answer.split('-');
+    var ans = [];
+
+    if(answer.length == 1) {
+     ans.push("*");
+     ans.push("*");
+     ans.push(answer[0]);
+    }
+    else if(answer.length == 2) {
+     ans.push("*");
+     ans.push(answer[0]);
+     ans.push(answer[1]);
     }
     else {
-	    log('You have excluded: "', answer, '"');
-	    answer = answer.split('-');
-        var ans = [];
-        if(answer.length == 1) {
-          ans.push("*");
-          ans.push("*");
-          ans.push(answer[0]);
-        }
-        else if(answer.length == 2) {
-          ans.push("*");
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-        }
-        else {
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-          ans.push(answer[2]);
-        }
-    log(ans);
+     ans.push(answer[0]);
+     ans.push(answer[1]);
+     ans.push(answer[2]);
+    }
 
-		let status = false;
-  	for (let i = 0; i < citiesList.length; i++) {
-		    if ((citiesList[i]["Country Name"].toUpperCase() == ans[2].toUpperCase() || ans[2]=='*') &&
-		    	(citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
-		    	(citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
-		    	){
-		        status = true;
-		    }
-		 }
-		if(status) {
-	  		data.distributor.includes.push(ans);
-		}
-		else {
-			log('Enter valid region!! ');
-		}
-	  getExcludedArea();
-	}
-  });
+    let status = false;
+    for (let i = 0; i < citiesList.length; i++) {
+	    if ((citiesList[i]["Country Name"].toUpperCase() == ans[2].toUpperCase() || ans[2]=='*') &&
+	  	   (citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
+	  	   (citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
+	  	 ){
+		      status = true;
+	    }
+	  }
+	  if (status) {
+ 		   data.distributor.excludes.push(ans);
+       log(ans);
+	  }
+	  else {
+		    log('Entered region is not valid. Please refer the "cities.csv" for the list of valid regions!');
+	  }
+   getExcludedArea();
+  }
+ });
 };
 
 var addSubDist = function () {
@@ -123,145 +131,192 @@ var addSubDist = function () {
 	data.distributor.subordinates.subDistributorCount.includes =[];
 	data.distributor.subordinates.subDistributorCount.excludes =[];
 
-  rl.question('Do you want to add subordinates: ', function (answer) {
-    if (answer == 'yes') {
-		    getIncludedAreaSubordinates(subDistributorCount);
-    }
-    else {
-    	return rl.close();
-	   }
-  });
+  rl.question('\nDo you want to add subordinates (yes/no): ', function (answer) {
+  if (answer == 'yes') {
+		  getIncludedAreaSubordinates(subDistributorCount);
+  }
+  else {
+  	return rl.close();
+	 }
+ });
 };
 
 var getIncludedAreaSubordinates = function (subDistributorCount) {
-  rl.question('Enter regions of sub Distributor to be INCLUDED: ', function (answer) {
-    if (answer == 'next') {
-		getExcludedAreaSubordinates(subDistributorCount);
+ rl.question('\nEnter regions of sub-Distributor to be INCLUDED: ', function (answer) {
+  if (answer == 'next') {
+		  getExcludedAreaSubordinates(subDistributorCount);
+  }
+  else if(answer == 'exit') {
+    log('\nExit.\nThank you!\n');
+    rl.close();
+  }
+  else {
+	  log('You are going to include: "', answer, '"');
+	  let permission = false;
+	  answer = answer.split('-');
+    var ans = [];
+
+    if(answer.length == 1) {
+     ans.push("*");
+     ans.push("*");
+     ans.push(answer[0]);
+    }
+    else if(answer.length == 2) {
+     ans.push("*");
+     ans.push(answer[0]);
+     ans.push(answer[1]);
     }
     else {
-	    log('You are going to include: "', answer, '"');
-	    let permission = false;
-	    answer = answer.split('-');
-	 	    answer = answer.split('-');
-        var ans = [];
-        if(answer.length == 1) {
-          ans.push("*");
-          ans.push("*");
-          ans.push(answer[0]);
-        }
-        else if(answer.length == 2) {
-          ans.push("*");
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-        }
-        else {
-          ans.push(answer[0]);
-          ans.push(answer[1]);
-          ans.push(answer[2]);
-        }
-        log(ans);
-	    city = ans[0];
-      state = ans[1];
-      country = ans[2];
-
-	  	data.distributor.includes.map(included => {
-		  if ( included[2] ===  country ) {
-		  	if(included[1] ===  '*') {
-		  		return permission = true;
-		  	}
-		  	if ( included[1] ===  state ) {
-		  		if ( included[0] ===  city ||  included[0] ===  '*') {
-		  			return permission = true;
-		  		}
-		  	}
+     ans.push(answer[0]);
+     ans.push(answer[1]);
+     ans.push(answer[2]);
+    }
+    log(ans);
+    let status = false;
+  	 for (let i = 0; i < citiesList.length; i++) {
+		    if ((citiesList[i]["Country Name"].toUpperCase() == ans[2].toUpperCase() || ans[2]=='*') &&
+		  	   (citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
+		  	   (citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
+		  	 ){
+		      status = true;
+		    }
 		  }
 
-		})
-		if(permission) {
-	  		data.distributor.subordinates.subDistributorCount.includes.push(ans);
-	  	}
-	  	else {
-	  		console.log("Permission Denied! The subordinate can not distribute in the above mentioned region");
-	  	}
-	    getIncludedAreaSubordinates();
-	   }
-  });
+	    city = ans[0];
+		  state = ans[1];
+		  country = ans[2];
+	 	  data.distributor.includes.map(included => {
+		     if ( included[2] === country ) {
+		 	     if(included[1] === '*') {
+		 		       return permission = true;
+		 	      }
+		 	      if ( included[1] === state ) {
+		 		        if ( included[0] === city || included[0] === '*') {
+		 			          return permission = true;
+		 		        }
+		 	      }
+		     }
+    })
+		if(status && permission) {
+	 		data.distributor.subordinates.subDistributorCount.includes.push(ans);
+	 	}
+	 	else if(!status){
+	 		log('\nEntered region is not valid. Please refer the "cities.csv" for the list of valid regions!');
+	 	}
+	 	else {
+	 		log("\nPermission Denied! The subordinate can not distribute in the above mentioned region!");
+	 	}
+	  getIncludedAreaSubordinates();
+	}
+ });
 };
 
 var getExcludedAreaSubordinates = function (subDistributorCount) {
-  rl.question('Enter regions of sub Distributor to be EXCLUDED: ', function (answer) {
-    if (answer == 'next') {
-		verifyPermission(subDistributorCount);
+ rl.question('\nEnter regions of sub-Distributor to be EXCLUDED: ', function (answer) {
+  if (answer == 'next') {
+    log("Sub-Distributor added successfully!");
+    rl.close();
+  }
+  else if(answer == 'exit') {
+    log('\nExit.\nThank you!\n');
+    rl.close();
+  }
+  else {
+	  log('You are going to exclude: "', answer, '"');
+	  answer = answer.split('-');
+    var ans = [];
+    if(answer.length == 1) {
+     ans.push("*");
+     ans.push("*");
+     ans.push(answer[0]);
+    }
+    else if(answer.length == 2) {
+     ans.push("*");
+     ans.push(answer[0]);
+     ans.push(answer[1]);
     }
     else {
-	    log('You have excluded: "', answer, '"');
-	    answer = answer.split('-');
-	  	data.distributor.subordinates.subDistributorCount.excludes.push(answer);
-	    getExcludedAreaSubordinates();
+     ans.push(answer[0]);
+     ans.push(answer[1]);
+     ans.push(answer[2]);
+    }
+
+    let status = false;
+  	for (let i = 0; i < citiesList.length; i++) {
+		  if ((citiesList[i]["Country Name"].toUpperCase() == ans[2].toUpperCase() || ans[2]=='*') &&
+		  	(citiesList[i]["Province Name"].toUpperCase() == ans[1].toUpperCase() || ans[1]=='*') &&
+		  	(citiesList[i]["City Name"].toUpperCase() == ans[0].toUpperCase() || ans[0]=='*')
+		  	){
+		    status = true;
+		  }
+		}
+		if(status) {
+	 		data.distributor.subordinates.subDistributorCount.excludes.push(answer);
+      log(ans);
+		}
+		else {
+			log('\nEntered region is not valid. Please refer the "cities.csv" for the list of valid regions!');
+		}
+	  getExcludedAreaSubordinates();
 	}
-  });
-};
-var verifyPermission = function (subDistributorCount) {
-	console.log('%o',data);
-	console.log('%o',child);
-	console.log(subDistributorCount);
-	rl.close();
+ });
 };
 
 var getResult = function () {
-  rl.question('Enter the question: ', function (question) {
-  	if (question == 'next') {
-    	console.log(data);
-    	addSubDist();
+	rl.question('\nEnter the region to check for the permission: ', function (question) {
+ 	if (question == 'next') {
+  	addSubDist();
+  }
+  else if(question == 'exit'){
+    log('\nExit.\nThank you!\n');
+  	rl.close();
+  }
+ 	else {
+		let inputarray, country, state, city, flag;
+	  inputarray = question.split('-');
+
+		city = inputarray[0];
+		state = inputarray[1];
+		country = inputarray[2];
+
+		flag = false;
+
+		data.distributor.includes.map( included => {
+		 if ( included[2] === country ) {
+		 	if(included[1] === '*') {
+		 		return flag = true;
+		 	}
+		 	if ( included[1] === state ) {
+		 		if ( included[0] === city || included[0] === '*') {
+		 			return flag = true;
+		 		}
+		 	}
     }
-    else if(question == 'exit'){
-    	rl.close();
-    }
-  	else {
-      let inputarray, country, state, city, flag;
-	    inputarray = question.split('-');
-	    console.log(inputarray);
+		})
 
-      city = inputarray[0];
-      state = inputarray[1];
-      country = inputarray[2];
-      flag = false;
+		data.distributor.excludes.map(excluded => {
+		 	if ( excluded[2] === country ) {
+				if ( excluded[1] === '*' ) {
+					return flag = false;
+				}
+				else {
+					if (excluded[1] === state) {
+						if ( excluded[0] === city || excluded[0] === '*') {
+							return flag = false;
+						}
+					}
+				}
+			}
+		})
 
-      data.distributor.includes.map(included => {
-      if (included[2] === country) {
-        if(included[1] === '*') {
-      	   return flag = true;
-         }
-         if(included[1] === state) {
-      	    if (included[0] === city ||  included[0] === '*') {
-      		      return flag = true;
-            }
-         }
-		  }
-		  else {
-          // do nothing
-		  }
-		});
-
-  data.distributor.excludes.map(excluded => {
-    if (excluded[2] === country ) {
-        if (excluded[1] === '*') {
-            return flag = false;
-        }
-        else {
-            if (excluded[1] === state) {
-              if (excluded[0] === city || excluded[0] === '*') {
-                  return flag = false;
-              }
-            }
-        }
-    }
-  })
-
-	flag ? console.log('yes') : console.log('no');
-	}
-  });
+		flag
+     ? log('\n Permission: *** YES ***\n')
+     : log('\n Permission: *** NO ***\n');
+     rl.close();
+	 }
+ });
 };
-
-// Starting point of the program
+log("------------------------------------------------------");
+log("(Type \"next\" to skip and \"exit\" to quit at anytime)");
+log("------------------------------------------------------");
 getIncludedArea();
